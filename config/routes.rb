ceda0218619af748de50afc90eb9a82cdf560e4e
nousi <nousidayo@gmail.com>
2019-10-01 00:31:21 +0900

    Githubの操作を理解するためにテスト
    ブランチ切り替えテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "messages#index"
 
+  resources :messages, only: [:index, :create]
+  resources :user, only: [:edit, :update]
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
